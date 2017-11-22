ce050a998156eff86a278668838f59e5b03de9bf
georgew <wgeorge@outlook.com>
2017-11-21 22:38:12 -0800

    moved stuff around

--- public/contentsToJSON.py
+++
@@ -1,8 +0,0 @@
-import os
-
-outfile = open('photography.json', 'w') 
-start_path = '.'
-for path,dirs,files in os.walk(start_path):
-    for filename in files:
-        outfile.write(os.path.join(path,filename))
-
